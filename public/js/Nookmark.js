646cc37c877862eda01f4b3ff07300d81dce56a3
to <9795+to@users.noreply.github.com>
2026-02-07 22:19:06 +0900

    Nookmarkをclassからconstへ変更

--- public/js/Nookmark.js
+++ public/js/Nookmark.js
@@ -1,28 +1,30 @@
-class Nookmark {
-	static apiBase = '/api/bookmarks';
+const Nookmark = {
+	apiBase: '/api/bookmarks',
 
-	static async getTags() {
+	async getTags() {
 		const res = await fetch('json/tags.json');
 		return (await res.json()).tags
 			.sort((a, b) => a.length - b.length || a.localeCompare(b));
-	}
+	},
 
-	static async getBookmark(id) {
+	async getBookmark(id) {
 		const res = await fetch(`${this.apiBase}/${id}`);
 		if (!res.ok)
 			throw new Error('Failed to load bookmark');
-
 		return this._populate(await res.json());
-	}
+	},
 
-	static async findByUrl(url) {
+	async findByUrl(url) {
 		const res = await fetch(`${this.apiBase}?${new URLSearchParams({ url })}`);
 		const json = await res.json();
 		return json && this._populate(json);
-	}
+	},
 
-	static async updateBookmark(bookmark) {
-		const url = bookmark.id ? `${this.apiBase}/${bookmark.id}` : this.apiBase;
+	async updateBookmark(bookmark) {
+		const url = bookmark.id ?
+			`${this.apiBase}/${bookmark.id}` :
+			this.apiBase;
+
 		const res = await fetch(url, {
 			method: 'POST',
 			headers: { 'Content-Type': 'application/json' },
@@ -34,34 +36,34 @@ class Nookmark {
 
 		if (!res.ok)
 			throw new Error('Update failed');
-	}
+	},
 
-	static async deleteBookmark(id) {
+	async deleteBookmark(id) {
 		const res = await fetch(`${this.apiBase}/${id}`, { method: 'DELETE' });
 		if (!res.ok)
 			throw new Error('Delete failed');
-	}
+	},
 
-	static async getBookmarks() {
+	async getBookmarks() {
 		const res = await fetch(this.apiBase);
 		return (await res.json()).map(this._populate);
-	}
+	},
 
-	static async search({ tags, query, fields, sortBy }) {
+	async search({ tags, query, fields, sortBy }) {
 		const res = await fetch(`/api/search?${new URLSearchParams({
 			query, fields, sortBy,
 			...this._separateRating(tags),
 		})}`);
 		return (await res.json()).map(this._populate);
-	}
+	},
 
-	static _populate(r) {
+	_populate(r) {
 		r.created_at = new Date(r.created_at);
 		r.updated_at = new Date(r.updated_at);
 		return r;
-	}
+	},
 
-	static _separateRating(tags) {
+	_separateRating(tags) {
 		let rating = null;
 		const filtered = tags.filter(t => {
 			if (!/^\d$/.test(t))
@@ -69,5 +71,5 @@ class Nookmark {
 			rating = (t > rating) ? +t : rating;
 		});
 		return { tags: filtered, rating };
-	}
-}
+	},
+};
