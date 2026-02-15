4eb2e9dc7783e2cff2e9e3d270b84d0f32d3f322
to <9795+to@users.noreply.github.com>
2026-02-15 09:03:57 +0900

    feat: 編集画面を開く機能を追加

--- tools/browser-extension/extension/background.js
+++ tools/browser-extension/extension/background.js
@@ -1,4 +1,72 @@
 const BASE = 'http://localhost:3000';
+const WINDOW_WIDTH = 500;
+const WINDOW_HEIGHT = 480;
+const WINDOW_MARGIN = 25;
+
+chrome.action.onClicked.addListener(tab => openUpdatePage(tab));
+
+chrome.commands.onCommand.addListener(async command => {
+	if (command === 'open-update-page') {
+		const [tab] = await chrome.tabs.query({
+			active: true,
+			currentWindow: true,
+		});
+		openUpdatePage(tab);
+	}
+});
+
+chrome.runtime.onInstalled.addListener(() => {
+	chrome.contextMenus.create({
+		id: 'search-nookmark',
+		title: 'Search Nookmark for "%s"',
+		contexts: ['selection'],
+	});
+});
+
+chrome.contextMenus.onClicked.addListener(info => {
+	if (info.menuItemId === 'search-nookmark') {
+		chrome.tabs.create({
+			url: `${BASE}/?query=${encodeURIComponent(info.selectionText)}`,
+		});
+	}
+});
+
+chrome.tabs.onUpdated.addListener((tabId, info, tab) => {
+	if (info.status === 'complete')
+		checkUrl(tabId, tab.url);
+});
+
+async function openUpdatePage(tab) {
+	const [{ result: html }] = await chrome.scripting.executeScript({
+		target: { tabId: tab.id },
+		func: () => document.documentElement.outerHTML,
+	});
+
+	const display = await getCurrentDisplay();
+	const area = display.workArea;
+
+	const updateWin = await chrome.windows.create({
+		url: `${BASE}/update.html`
+			+ `?url=${encodeURIComponent(tab.url)}`
+			+ `&title=${encodeURIComponent(tab.title)}`,
+		type: 'popup',
+		width: WINDOW_WIDTH,
+		height: WINDOW_HEIGHT,
+		left: area.left + area.width - WINDOW_WIDTH - (WINDOW_MARGIN + 10),
+		top: area.top + area.height - (WINDOW_HEIGHT - 20) - WINDOW_MARGIN,
+	});
+	chrome.tabs.onUpdated.addListener(function listener(id, info) {
+		if (id !== updateWin.tabs[0].id || info.status !== 'complete')
+			return;
+		chrome.tabs.onUpdated.removeListener(listener);
+
+		chrome.scripting.executeScript({
+			target: { tabId: id },
+			func: html => document.querySelector('#html').value = html,
+			args: [html],
+		});
+	});
+}
 
 async function checkUrl(tabId, url) {
 	// 特殊なページは除外する
@@ -23,23 +91,11 @@ async function checkUrl(tabId, url) {
 	}
 }
 
-chrome.runtime.onInstalled.addListener(() => {
-	chrome.contextMenus.create({
-		id: 'searchNookmark',
-		title: 'Search Nookmark for "%s"',
-		contexts: ['selection'],
-	});
-});
-
-chrome.contextMenus.onClicked.addListener(info => {
-	if (info.menuItemId === 'searchNookmark') {
-		chrome.tabs.create({
-			url: `${BASE}/?query=${encodeURIComponent(info.selectionText)}`,
-		});
-	}
-});
-
-chrome.tabs.onUpdated.addListener((tabId, info, tab) => {
-	if (info.status === 'complete')
-		checkUrl(tabId, tab.url);
-});
+async function getCurrentDisplay() {
+	const currentWin = await chrome.windows.getCurrent();
+	const centerX = currentWin.left + currentWin.width / 2;
+	const displays = await chrome.system.display.getInfo();
+	return displays.find(d =>
+		(centerX >= d.workArea.left) && (centerX < d.workArea.left + d.workArea.width),
+	) || displays[0];
+}
