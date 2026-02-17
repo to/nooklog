b5a96373e1503fb2634267731cc5ef2b0ab4b4ec
to <9795+to@users.noreply.github.com>
2026-02-17 09:54:10 +0900

    misc:

--- public/js/init.js
+++ public/js/init.js
@@ -1,3 +1,4 @@
+// Chrome拡張ではセキュリティポリシーによりインラインスクリプトは使えない
 document.documentElement.classList.add(localStorage.theme ??
 	(window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'));
 document.documentElement.lang = navigator.language.slice(0, 2);
