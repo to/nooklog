eeeb9aa580965558c8ceef3f8ae90f17fbeafb5e
to <9795+to@users.noreply.github.com>
2026-02-26 16:58:33 +0900

    refactor: レーティングの実装方法を検索結果に合わせる

--- public/js/RatingInput.js
+++ public/js/RatingInput.js
@@ -4,56 +4,56 @@ class RatingInput {
 		this.value = parseInt(this.els.input.value) || 0;
 
 		this._init();
-		this._render('active');
+		this._bindEvents();
 	}
 
 	_init() {
 		this.els.input.classList.add('none');
 		this.els.rating = document.createElement('div');
 		this.els.rating.className = 'input-rating';
+		this.els.rating.dataset.rating = this.value;
 
-		this.els.stars = [...Array(5)].map((_, i) => this._createStar(i + 1));
-		this.els.rating.append(...this.els.stars);
+		this.els.rating.innerHTML = [1, 2, 3, 4, 5].reduceRight((acc, i) =>
+			`<span class="icon btn-text${i == this.value ? ' active' : ''}" data-rating="${i}">` +
+			`star${acc}` +
+			'</span>', '');
 
 		this.els.input.after(this.els.rating);
 	}
 
-	_createStar(index) {
-		const span = document.createElement('span');
-		span.className = 'icon btn-text';
-		span.textContent = 'star';
+	_bindEvents() {
+		this.els.rating.addEventListener('click', e => {
+			const star = e.target.closest('.icon');
+			if (!star)
+				return;
 
-		span.addEventListener('click', () => {
-			this.value = (this.value === index) ? 0 : index;
+			const rating = +star.dataset.rating;
+			this.value = (this.value === rating) ? 0 : rating;
 			this.els.input.value = this.value;
+			this.els.rating.dataset.rating = this.value;
+			this.els.rating.classList.remove('hover');
+
+			this.els.rating.querySelectorAll('.icon').forEach(s =>
+				s.classList.toggle('active', s.dataset.rating == this.value));
+
 			this.els.input.dispatchEvent(new Event('change', { bubbles: true }));
-			this._render('active');
-			this._render('hover', 0);
 		});
 
-		span.addEventListener('mouseenter', () => {
-			this._render('active', 0);
-			this._render('hover', index);
-		});
-		span.addEventListener('mouseleave', () => {
-			this._render('active');
-			this._render('hover', 0);
-		});
+		this.els.rating.addEventListener('mouseover', () =>
+			this.els.rating.classList.add('hover'));
 
-		return span;
-	}
-
-	_render(cls, index) {
-		index = index ?? this.value;
-		this.els.stars.forEach((s, i) => {
-			s.classList.toggle(cls, index >= (i + 1));
+		this.els.rating.addEventListener('mouseout', e => {
+			if (!this.els.rating.contains(e.relatedTarget))
+				this.els.rating.classList.remove('hover');
 		});
 	}
 
 	setValue(val) {
 		this.value = parseInt(val) || 0;
 		this.els.input.value = this.value;
-		this._render('active');
+		this.els.rating.dataset.rating = this.value;
+		this.els.rating.querySelectorAll('.icon').forEach(s =>
+			s.classList.toggle('active', s.dataset.rating == this.value));
 	}
 
 	getValue() {
